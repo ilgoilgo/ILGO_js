9fec73cabe15712ccb1b5cb62311775d9b47f8b3
Ilia <okypo4ek@bk.ru>
2018-08-16 15:33:51 +1000

    omg

---
+++ 02_selectors/js/script.js
@@ -0,0 +1,11 @@
+console.log('Your are at ' +window.location);
+
+const byTag = document.getElementsByTagName('div');
+const byClass = document.getElementsByClassName('myClass');
+const byId = document.getElementById('myId');
+
+console.log(byTag, byClass, byId);
+
+const allBySelector = document.querySelectorAll('.coolClass.mine');
+const firstBySelector = document.querySelector('.coolClass');
+console.log(allBySelector, firstBySelector);
